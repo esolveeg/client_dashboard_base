4bb7e32e5628ad1d047d9a8cdf6305b1f0f04ef0
ahmed ashraf <ahmed.ashraf.devv@gmail.com>
2021-07-08 22:44:21 +0200

    added navbar button and merged app bar with app nav

--- components/layouts/AppNav.vue
+++ components/layouts/AppNav.vue
@@ -1,29 +1,49 @@
 <template>
-  <v-navigation-drawer
-    v-model="drawer"
-    :clipped="true"
-    :right="$vuetify.rtl"
-    :mini-variant="true"
-    fixed
-    app
-  >
-    <v-list>
-      <v-list-item
-        v-for="(item, i) in items"
-        :key="i"
-        :to="item.to"
-        router
-        exact
-      >
-        <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-action>
-        <!-- <v-list-item-content>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="true"
+      :right="$vuetify.rtl"
+      :mini-variant="true"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <!-- <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content> -->
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="!$vuetify.rtl"
+      :clipped-right="$vuetify.rtl"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="'El Nozom'" />
+      <v-spacer />
+      <v-icon
+        class="pointer mr-4"
+        @click.prevent="switchLanguage"
+      >mdi-earth</v-icon>
+      <v-icon
+        class="pointer"
+        @click.prevent="switchTheme"
+      >mdi-weather-night</v-icon>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
-  <v-app dark class="relative">
+  <v-app
+    dark
+    class="relative"
+  >
     <layouts-app-loader v-show=" isLoading" />
-    <layouts-app-nav   v-show=" !isLoading" />
-    <layouts-app-bar   v-show=" !isLoading" />
-    <v-main v-show=" !isLoading" >  
+    <layouts-app-nav v-show=" !isLoading" />
+    <!-- <layouts-app-bar   v-show=" !isLoading" /> -->
+    <v-main v-show=" !isLoading">
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <layouts-app-footer   v-show=" !isLoading" />
+    <layouts-app-footer v-show=" !isLoading" />
   </v-app>
 </template>
 
@@ -18,12 +21,11 @@ import { initApp } from '@/utils/helpers/Global.js'
 export default {
   computed: {
     ...mapGetters({
-      isLoading: 'ui/isLoading',
-    }),
-  },
-  created() {
-    initApp(this).then(() => {
+      isLoading: 'ui/isLoading'
     })
   },
+  created() {
+    initApp(this).then(() => {})
+  }
 }
 </script>
